2ac1bb80c3cc50678244dd296eccc3fb5e1c4849
Shinnolo <simo.28.09o@gmai.com>
2021-02-22 15:30:11 +0100

    painter innit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/style.scss":
+/*!************************!*\
+  !*** ./src/style.scss ***!
+  \************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n  ╷\n1 │ @import \"partials/variables\";\r\n  │         ^^^^^^^^^^^^^^^^^^^^\n  ╵\n  C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\src\\style.scss 1:9  root stylesheet\n    at processResult (C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\node_modules\\webpack\\lib\\NormalModule.js:598:19)\n    at C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\node_modules\\webpack\\lib\\NormalModule.js:692:5\n    at C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\node_modules\\sass\\sass.dart.js:91729:16)\n    at _render_closure1.call$2 (C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\node_modules\\sass\\sass.dart.js:80373:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\node_modules\\sass\\sass.dart.js:27269:18)\n    at _FutureListener.handleError$1 (C:\\Users\\user\\Desktop\\Boolean\\sass-painter\\node_modules\\sass\\sass.dart.js:25797:19)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/style.scss"]();
+/******/ 	
+/******/ })()
+;
